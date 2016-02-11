7c1c430a0919af8d0f2eb0e873b83858edcba570
VLIGS <liudmila.veshneva@gmail.com>
2016-02-11 21:44:10 +0000

    refactored some analyser tests to be length flexible

--- src/AnalyserTests.java
+++ src/AnalyserTests.java
@@ -16,16 +16,29 @@ public class AnalyserTests {
 
     @Test
     public void test_analyseGuess_with_AllMatchingPegsAllMatchingPosition() {
-        guess = new LineImpl(new PegImpl(code.getPeg(0).getPegColour()), new PegImpl(code.getPeg(1).getPegColour()), new PegImpl(code.getPeg(2).getPegColour()), new PegImpl(code.getPeg(3).getPegColour()));
-        Line expected = new LineImpl(new PegImpl(Feedback.getExactMatch()), new PegImpl(Feedback.getExactMatch()), new PegImpl(Feedback.getExactMatch()), new PegImpl(Feedback.getExactMatch()));
+        guess = new LineImpl();
+        Line expected = new LineImpl();
+        for (int i = 0; i<code.numberOfPegs(); i++){
+            guess.addPeg(new PegImpl(code.getPeg(i).getPegColour()));
+        }
+        for (int i = 0; i<code.numberOfPegs(); i++){
+            expected.addPeg(new PegImpl(Feedback.getExactMatch()));
+        }
         Line actual = analyser.analyseGuess(guess, code);
         assertEquals(expected.toString(), actual.toString());
     }
 
     @Test
     public void test_analyseGuess_with_AllMatchingPegsNotMatchingPosition () {
-        guess = new LineImpl(new PegImpl(code.getPeg(3).getPegColour()),new PegImpl(code.getPeg(2).getPegColour()), new PegImpl(code.getPeg(1).getPegColour()), new PegImpl(code.getPeg(0).getPegColour()));
-        Line expected = new LineImpl(new PegImpl(Feedback.getSymbolMatch()),new PegImpl(Feedback.getSymbolMatch()), new PegImpl(Feedback.getSymbolMatch()), new PegImpl(Feedback.getSymbolMatch()));
+        Line expected = new LineImpl();
+        guess = new LineImpl();
+
+        for (int i = code.numberOfPegs()-1; i>=0; i--){
+            guess.addPeg(new PegImpl(code.getPeg(i).getPegColour()));
+        }
+        for (int i = 0; i<code.numberOfPegs(); i++){
+            expected.addPeg(new PegImpl(Feedback.getSymbolMatch()));
+        }
         Line actual = analyser.analyseGuess(guess,code);
         assertEquals(expected.toString(),actual.toString());
     }
@@ -46,14 +59,21 @@ public class AnalyserTests {
 
     @Test
     public void test_IfWon_with_AllMatchingPegsAllMatchingPosition () {
-        guess = new LineImpl(new PegImpl(code.getPeg(0).getPegColour()), new PegImpl(code.getPeg(1).getPegColour()), new PegImpl(code.getPeg(2).getPegColour()), new PegImpl(code.getPeg(3).getPegColour()));
+        guess = new LineImpl();
+        for (int i = 0; i<code.numberOfPegs(); i++){
+            guess.addPeg(new PegImpl(code.getPeg(i).getPegColour()));
+        }
         assertEquals(true, analyser.checkIfWon(analyser.analyseGuess(guess,code),code.numberOfPegs()));
     }
 
     @Test
     public void test_IfWon_with_OnlyOneMatchingPegOneMatchingPosition () {
         Peg notMatchingPeg = new PegImpl("O");
-        guess = new LineImpl(new PegImpl(code.getPeg(0).getPegColour()), notMatchingPeg, notMatchingPeg, notMatchingPeg);
+        guess = new LineImpl();
+        for (int i = 0; i<code.numberOfPegs(); i++){
+            guess.addPeg(notMatchingPeg);
+        }
+        guess.setPeg(0,code.getPeg(0).getPegColour());
         assertEquals(false, analyser.checkIfWon(analyser.analyseGuess(guess,code),code.numberOfPegs()));
     }
 
